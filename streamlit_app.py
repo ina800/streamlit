6070a6ce7b29f3b3e297b78e6874060e76498186
ina800 <msykume@gmail.com>
2023-05-07 18:18:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.title('これがタイトル')
+st.title('Streamlitの練習')
+# やりたいこと
+# 利用者が自分のデータをアップロードし、加工、可視化する。
+# データのアップロード部分
+# 加工部分
+# 可視化部分
+# intaractive性
+
+#　まずは基本から
+# csvファイルをアップロードしグラフ化するだけのプログラム
